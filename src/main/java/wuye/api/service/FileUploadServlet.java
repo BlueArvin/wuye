1345876fd876d3e15f9ee010b41ccb8eafc95930
Lu Jinfei <lujinfeifly@aliyun.com>
2017-02-21 13:54:00 +0800

    文件上传测试完成
    文件上传测试完成

--- src/main/java/wuye/api/service/FileUploadServlet.java
+++ src/main/java/wuye/api/service/FileUploadServlet.java
@@ -1,8 +1,11 @@
 package wuye.api.service;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import wuye.api.bean.RetBean;
 
@@ -19,25 +24,47 @@ public class FileUploadServlet {
 
 	@RequestMapping("uploadpic")
 	@ResponseBody
-    public String upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request) {  
+    public String upload(HttpServletRequest request,HttpServletResponse response) {  
   
-        System.out.println("开始");  
-        String path = request.getSession().getServletContext().getRealPath("upload");  
-        String fileName = file.getOriginalFilename();  
-        System.out.println(path);  
-        File targetFile = new File(path, fileName);  
-        if(!targetFile.exists()){  
-            targetFile.mkdirs();  
-        }  
-  
-        //保存  
-        try {  
-            file.transferTo(targetFile);  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-        // model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName);  
-  
-        return "result";  
+		//创建一个通用的多部分解析器  
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());  
+        //判断 request 是否有文件上传,即多部分请求  
+//        if(multipartResolver.isMultipart(request)){  
+            //转换成多部分request    
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;  
+            //取得request中的所有文件名  
+            Iterator<String> iter = multiRequest.getFileNames();  
+            while(iter.hasNext()){  
+                //记录上传过程起始时的时间，用来计算上传时间  
+                int pre = (int) System.currentTimeMillis();  
+                //取得上传文件  
+                MultipartFile file = multiRequest.getFile(iter.next());  
+                if(file != null){  
+                    //取得当前上传文件的文件名称  
+                    String myFileName = file.getOriginalFilename();  
+                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在  
+                    if(myFileName.trim() !=""){  
+                        System.out.println(myFileName);  
+                        //重命名上传后的文件名  
+                        String fileName = "demoUpload" + file.getOriginalFilename();  
+                        //定义上传路径  
+                        String path = "E:/upload/" + fileName;  
+                        File localFile = new File(path);  
+                        try {
+							file.transferTo(localFile);
+							return fileName;
+						} catch (IllegalStateException e) {
+
+						} catch (IOException e) {
+						}  
+                    }  
+                }  
+                //记录上传该文件后的时间  
+                int finaltime = (int) System.currentTimeMillis();  
+                System.out.println(finaltime - pre);  
+            }  
+            return "2"; 
+//        }  
+//        return "/success";  
     }  
 }
