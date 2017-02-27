1b7d14518a27ade2ad7d2c78a91a7ed40cb4b77e
ren7753366 <ren7753366@126.com>
2017-02-27 22:41:35 +0800

    Revert "后台页面添加"
    This reverts commit 2d0b1f9336bc71f47c42e363fce168543b850366.

--- src/main/java/wuye/manager/menu/dao/LoginDao.java
+++
@@ -1,7 +0,0 @@
-package wuye.manager.menu.dao;
-
-import wuye.manager.user.bean.UserBean;
-
-public interface LoginDao {
-	public UserBean checkUser(String userName,String passwd);
-}
--- src/main/java/wuye/manager/menu/logic/LoginLogic.java
+++
@@ -1,7 +0,0 @@
-package wuye.manager.menu.logic;
-
-import wuye.manager.user.bean.UserBean;
-
-public interface LoginLogic {
-	public UserBean checkUser(String loginName,String passwd);
-}
