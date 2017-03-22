78600054b7e443b788fac30d830d2f6500019456
Lu Jinfei <lujinfeifly@aliyun.com>
2017-03-22 20:59:29 +0800

    do
    do

--- src/main/java/wuye/api/task/TaskJob.java
+++ src/main/java/wuye/api/task/TaskJob.java
@@ -8,5 +8,9 @@ public class TaskJob {
     @Scheduled(cron = "30 3 * * 6 ?")
     public void job1() {   // 每周六的定时任务
         System.out.println("任务进行中。。。");
+        
+        // 进行本周的数据统计，这里是物业排名
+        
+        // 进行片区排名
     }
 }
